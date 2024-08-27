0ab498a1eee92d062ee2aaa3e4e6b52e84c35644
hazadus <hazadus7@gmail.com>
2024-08-27 21:33:37 +0300

    feat: Run external program to view HTML file.

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"os/exec"
+	"runtime"
 
 	"github.com/microcosm-cc/bluemonday"
 	"github.com/russross/blackfriday/v2"
@@ -26,7 +28,7 @@ const (
 	`
 )
 
-// parseContent переводит входные данные в формате Markdown в HTML.
+// parseContent переводит входные данные input в формате Markdown в HTML.
 func parseContent(input []byte) []byte {
 	output := blackfriday.Run(input)
 	body := bluemonday.UGCPolicy().SanitizeBytes(output)
@@ -45,10 +47,43 @@ func saveHTML(outputFileName string, data []byte) error {
 	return os.WriteFile(outputFileName, data, 0644)
 }
 
+// preview открывает файл filename стандартными средствами системы.
+func preview(filename string) error {
+	// В переменных будут установлены команда и параметры для запуска
+	// стандартного средства просмотра целевой ОС.
+	cmdName := ""
+	cmdParams := []string{}
+
+	switch runtime.GOOS {
+	case "darwin":
+		cmdName = "open"
+	case "linux":
+		cmdName = "xdg-open"
+	case "windows":
+		cmdName = "cmd.exe"
+		cmdParams = []string{"/C", "start"}
+	default:
+		return fmt.Errorf("OS not supported.")
+	}
+
+	cmdParams = append(cmdParams, filename)
+
+	// Найти исполняемый файл в PATH
+	cmdPath, err := exec.LookPath(cmdName)
+	if err != nil {
+		return err
+	}
+
+	// Открыть файл при помощи выбранной программы
+	return exec.Command(cmdPath, cmdParams...).Run()
+}
+
 // run выполняет основной функционал программы.
 // filename - Markdown-файл, который нужно просмотреть.
-// out - Writer, куда будет выведено имя временного файла с HTML.
-func run(filename string, out io.Writer) error {
+// out - Writer, куда будет выведено имя временного файла с HTML,
+// skipPreview позволяет пропустить запуск системной программы просмотра
+// файла.
+func run(filename string, out io.Writer, skipPreview bool) error {
 	input, err := os.ReadFile(filename)
 	if err != nil {
 		return err
@@ -68,11 +103,21 @@ func run(filename string, out io.Writer) error {
 	outputFileName := tempFile.Name()
 	fmt.Fprintln(out, outputFileName)
 
-	return saveHTML(outputFileName, html)
+	err = saveHTML(outputFileName, html)
+	if err != nil {
+		return err
+	}
+
+	if skipPreview {
+		return nil
+	}
+
+	return preview(outputFileName)
 }
 
 func main() {
 	filenameFlag := flag.String("file", "", "Файл в формате Markdown для просмотра")
+	skipPreviewFlag := flag.Bool("s", false, "Не запускать программу просмотра файла")
 	flag.Parse()
 
 	if *filenameFlag == "" {
@@ -80,7 +125,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	if err := run(*filenameFlag, os.Stdout); err != nil {
+	if err := run(*filenameFlag, os.Stdout, *skipPreviewFlag); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
